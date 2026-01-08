d109ded995df25331ec85fc4516330061cce2aea
Adityak087 <adityakute0087@gmail.com>
2026-01-08 23:27:41 +0530

    day 4

---
+++ DynamicProgramming/bitmasks/subsets.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i,st,n) for(int i=st;i<n;++i)
+#define all(x) (x).begin(),(x).end()
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vll vector<long long>
+#define pb push_back
+#define fastio() ios::sync_with_stdio(false);cin.tie(nullptr)
+const int MOD = 1000000007;
+
+
+void solve(){
+
+}
+int main() {
+    fastio();
+    int t;
+    cint>>t;
+    while(t--){
+        solve();
+    }
+}
